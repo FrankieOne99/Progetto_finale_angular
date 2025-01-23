c2f0a862a768359e6e952b7816f0373ad4c6f353
Francesco Labate <franco.labate19@gmail.com>
2025-01-23 16:20:44 +0100

    preparazione prenotazioni

---
+++ Angular_progettoFinale/prenotazioni-palestra/src/app/prenotazioni/nuova-prenotazione/nuova-prenotazione.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuova-prenotazione',
+  standalone: true,
+  imports: [],
+  templateUrl: './nuova-prenotazione.component.html',
+  styleUrl: './nuova-prenotazione.component.css'
+})
+export class NuovaPrenotazioneComponent {
+
+}
---
+++ Angular_progettoFinale/prenotazioni-palestra/src/app/prenotazioni/prenotazione/prenotazione.model.ts
@@ -0,0 +1,5 @@
+export interface Prenotazione {
+  id: number;
+  corso: string;
+  duedate: string;
+}
